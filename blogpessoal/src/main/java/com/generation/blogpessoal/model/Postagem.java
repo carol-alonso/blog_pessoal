28af6b851921f26d2b3d9a3d73cb5aac9aaf3149
carol-alonso <lacerdacarol01@gmail.com>
2022-02-17 15:00:27 -0300

    17fev ATUALIZAÇÕES

---
+++ blogpessoal/src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -0,0 +1,67 @@
+package com.generation.blogpessoal.model;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import org.hibernate.annotations.UpdateTimestamp;
+
+
+
+@Entity
+@Table(name = "tb_postagem")
+
+
+public class Postagem {
+	
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotBlank
+	@Size(min = 5, max = 100)
+	private String titulo;
+	
+	@NotBlank
+	@Size(min = 5, max = 500)
+	private String texto;
+	
+	@UpdateTimestamp
+	private LocalDateTime data;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+
+
+
+	
+	
+}
